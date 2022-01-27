020b1a3aad77780dd5aeb52deaae95139410f280
Kwame <gtwene001@st.ug.edu.gh>
2022-01-27 16:46:38 +0000

    ThirdWeb_Auth_applied

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,18 @@ export default function Home() {
   const { address, connectWallet } = useWeb3();
   return (
     <Wrapper>
-      <button onClick={() => connectWallet("injected")}>Connect Wallet</button>
+      {address ? (
+        <h2>{address}</h2>
+      ) : (
+        <WalletConnect>
+          <Button onClick={() => connectWallet("injected")}>
+            Connect Wallet
+          </Button>
+          <Details>
+            You need Chrome to be <br /> able to run this app
+          </Details>
+        </WalletConnect>
+      )}
     </Wrapper>
   );
 }
@@ -18,4 +29,35 @@ const Wrapper = styled.div`
   color: white;
   display: grid;
   place-items: center;
+`;
+
+const WalletConnect = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.div`
+  // flex: 0;
+
+  border: 1px solid #282b2f;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  background-color: #3773f5;
+  color: #000;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Details = styled.div`
+  font-size: 1.2rem;
+  text-align: center;
+  margin-top: 1rem;
+  font-weight: 500;
+  color: #282b2f;
 `;
